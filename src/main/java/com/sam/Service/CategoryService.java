a7ffd0fb732081e1eede674427de840fbb0a6d96
Sambhav Agarwal <agarwalsambhav800@gmail.com>
2025-01-29 19:27:12 +0530

    created category service and api

--- src/main/java/com/sam/Service/CategoryService.java
+++ src/main/java/com/sam/Service/CategoryService.java
@@ -1,4 +1,16 @@
 package com.sam.Service;
 
+import com.sam.model.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+
+
+    Category createCategory(String name, Long userId) throws Exception;
+
+    List<Category> findCategoryByRestaurantId(Long userId) throws Exception;
+
+    Category findCategoryById(Long id) throws Exception;
+
 }
